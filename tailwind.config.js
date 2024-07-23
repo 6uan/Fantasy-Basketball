d07739cab019a05818edcb4ea322aee7a57c32a4
6uan <6uannn@gmail.com>
2024-07-22 23:00:54 -0400

    feat: implemented wireframe for landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./templates/**/*.html", "./static/js/**/*.js"],
+  content: [
+    "./templates/**/*.html", // Watch all HTML files in the templates directory and subdirectories
+    "./static/**/*.css", // Watch all CSS files in the static directory and subdirectories
+    "./static/js/**/*.js", // Watch all JS files in the static/js directory and subdirectories
+  ],
   theme: {
     extend: {},
   },
