1db35d3d8d493ec2c6d182a60b3ab3c22121425f
6uan <6uannn@gmail.com>
2024-07-23 20:36:16 -0400

    feat: major frontend changes (some UI bugs in the rendering of usernav HTML when page refreshes)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,10 +6,30 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-
 @app.route("/login")
 def login():
     return render_template('login.html')
+
+@app.route("/myteam")
+def myteam():
+    return render_template('nav/myteam.html')
+
+@app.route("/playershop")
+def playershop():
+    return render_template('nav/playershop.html')
+
+@app.route("/playerstats")
+def playerstats():
+    return render_template('nav/playerstats.html')
+
+@app.route("/leaderboards")
+def section2():
+    return render_template('nav/leaderboards.html')
+
+@app.route("/gameschedule")
+def gameschedule():
+    return render_template('nav/gameschedule.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
