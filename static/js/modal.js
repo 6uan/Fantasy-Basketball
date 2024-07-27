1b20411ce9d4b701ecc7d3e64ab423ab5490dd12
6uan <6uannn@gmail.com>
2024-07-26 23:32:47 -0400

    refactor: both signup/login are in same modal (functonal but missing invalid handling and checking if user is already signed in)

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,24 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var modal = document.getElementById("loginModal");
-  var span = document.getElementsByClassName("close")[0];
+// Toggle between signup and login forms
+document.getElementById("signupTab").addEventListener("click", function () {
+  document.getElementById("signupForm").classList.remove("hidden");
+  document.getElementById("loginForm").classList.add("hidden");
+  document.getElementById("signupTab").classList.add("border-b-2");
+  document.getElementById("loginTab").classList.remove("border-b-2");
+});
 
-  function showModal() {
-    modal.style.display = "block";
+document.getElementById("loginTab").addEventListener("click", function () {
+  document.getElementById("loginForm").classList.remove("hidden");
+  document.getElementById("signupForm").classList.add("hidden");
+  document.getElementById("loginTab").classList.add("border-b-2");
+  document.getElementById("signupTab").classList.remove("border-b-2");
+});
+
+// Toggle login modal visibility
+document.getElementById("loginButton").addEventListener("click", function () {
+  document.getElementById("loginModal").classList.remove("hidden");
+});
+
+// Close modal when clicking on the close button
+document.querySelectorAll(".close").forEach(function (element) {
+  element.addEventListener("click", function () {
+    document.getElementById("loginModal").classList.add("hidden");
+  });
+});
+
+// Close modal when clicking outside of the modal
+window.addEventListener("click", function (event) {
+  if (event.target == document.getElementById("loginModal")) {
+    document.getElementById("loginModal").classList.add("hidden");
   }
-
-  function hideModal() {
-    modal.style.display = "none";
-  }
-
-  span.onclick = function () {
-    hideModal();
-  };
-
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      hideModal();
-    }
-  };
-
-  document.getElementById("loginButton").addEventListener("click", showModal);
 });
