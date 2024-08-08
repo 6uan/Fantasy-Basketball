734a9b85fd4b39e0a029fa1449cd531cda224425
6uan <6uannn@gmail.com>
2024-08-07 20:50:30 -0400

    feat: landing page complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
   content: [
-    "./templates/**/*.html", // Watch all HTML files in the templates directory and subdirectories
-    "./static/**/*.css", // Watch all CSS files in the static directory and subdirectories
-    "./static/js/**/*.js", // Watch all JS files in the static/js directory and subdirectories
+    './templates/**/*.html', // Watch all HTML files in the templates directory and subdirectories
+    './static/**/*.css', // Watch all CSS files in the static directory and subdirectories
+    './static/js/**/*.js', // Watch all JS files in the static/js directory and subdirectories
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kanit: ['Kanit', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+    },
   },
   plugins: [],
-};
+}
